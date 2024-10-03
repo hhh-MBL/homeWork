93e70f535df8e3bd757df099b0d79f52a7120f0e
hhh-MBL <almondtor123@gmail.com>
2024-10-04 01:01:57 +0300

    Initial commit with project files

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const twilio = require('twilio');
 const cron = require('node-cron');
-const { sendInteractiveMessage } = require('./whatsappBot');  // Import the message sending function
+const path = require('path');
+const { sendInteractiveMessage } = require('./whatsappBot');
 
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Handle JSON data
+
+// Serve the HTML form on the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Serve the index.html file
+});
 
 // Route for submitting homework and sending a WhatsApp notification
 app.post('/submit-lesson', async (req, res) => {
